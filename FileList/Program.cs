fc69c2d0afe167e595a9b0c82be2c79f0bb8cd99
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:46:37 +0300

    Add project files.

---
+++ FileList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> paths = new List<string>();
+            //DirectoryInfo user = new DirectoryInfo(@"C:\Users\Lenovo\Pictures\Saved Pictures");
+            Console.WriteLine(Directory.GetLastWriteTime(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)));
+            Console.ReadLine();
+            Console.WriteLine(Directory.GetLastWriteTime(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)));
+            Console.ReadLine();
+        }
+    }
+
+    class Tree
+    {
+
+    }
+
+    class TreeItem
+    {
+        private string path;
+        private bool isFolder;
+        private List<TreeItem> treeItems = new List<TreeItem>();
+        private DateTime lastWriteTime;
+
+        public string Path
+        {
+            get { return path; }
+        }
+
+        public bool IsFolder
+        {
+            get { return isFolder; }
+        }
+
+        public List<TreeItem> Content
+        {
+            get { return treeItems; }
+        }
+
+        public DateTime LastWriteTime
+        {
+            get { return lastWriteTime; }
+        }
+    }
+}
